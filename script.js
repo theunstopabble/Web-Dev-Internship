f3e296f8b7386c5cec57f2978ba78736154f7fc7
Gautam Kumar <149867786+theunstopabble@users.noreply.github.com>
2025-12-09 21:51:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the button element
+const button = document.getElementById("alertBtn");
+
+// Add an event listener for the 'click' event
+button.addEventListener("click", function () {
+  // Display the alert message
+  alert("Hello! You have successfully completed Task 1 of the Internship.");
+});
